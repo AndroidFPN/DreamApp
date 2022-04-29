9391335064e2f38ba0689662a00543f46d260098
nedajavan747 <nedajavan747@gmail.com>
2022-04-29 16:06:47 +0430

    Add database.

---
+++ app/src/main/java/com/androidfpn/dreamapp/data/locale/db/AppDatabase.kt
@@ -0,0 +1,44 @@
+package com.androidfpn.dreamapp.data.locale.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.androidfpn.dreamapp.data.locale.dao.SoundCategoriesDao
+import com.androidfpn.dreamapp.data.locale.dao.SoundDao
+import com.androidfpn.dreamapp.data.locale.entity.Sound
+import com.androidfpn.dreamapp.data.locale.entity.SoundCategories
+import com.androidfpn.dreamapp.data.locale.entity.SoundsDeclaimers
+import com.androidfpn.dreamapp.data.locale.entity.SuggestCategories
+
+
+@Database(
+    entities = [Sound::class, SoundCategories::class, SoundsDeclaimers::class, SuggestCategories::class],
+    version = 1,
+    exportSchema = false
+)
+public abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun soundDao(): SoundDao
+    abstract fun soundCategoriesDao(): SoundCategoriesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
