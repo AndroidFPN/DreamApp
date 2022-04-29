b9858493aed637fc505a41af765aaacb27e2207f
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2022-04-29 15:50:28 +0430

    [FEATURE]:add navigation component for fragment

--- app/src/main/java/com/androidfpn/dreamapp/MainActivity.kt
+++ app/src/main/java/com/androidfpn/dreamapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.androidfpn.dreamapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController : NavController =Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
+        val bottomNavigationView: BottomNavigationView=findViewById(R.id.activity_main_bottom_navigation_view)
+//        NavigationUI.setupWithNavController(navController = navController)
     }
 }
