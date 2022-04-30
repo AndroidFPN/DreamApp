e90dbbbc5b456e515058db0fc5ae6253c0acff53
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2022-04-30 12:18:20 +0430

    [NEW]:add chips group to home page

--- app/src/main/java/com/androidfpn/dreamapp/screen/home/HomeFragment.kt
+++ app/src/main/java/com/androidfpn/dreamapp/screen/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.androidfpn.dreamapp.screen.home
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.androidfpn.dreamapp.R
+import com.androidfpn.dreamapp.databinding.HomeFragmentBinding
+import com.google.android.material.chip.Chip
 
 class HomeFragment : Fragment() {
 
@@ -15,12 +20,41 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var viewModel: HomeViewModel
+    private var _binding: HomeFragmentBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+        _binding = HomeFragmentBinding.inflate(inflater, container, false)
+        val view = binding?.root
+
+        setupChipsGroup()
+
+        return view
+
+    }
+
+    private fun setupChipsGroup() {
+        val items = arrayOf("خواب آرام", "صبح انرژیک", "مدیتیشن", "بی استرس")
+        items.forEach {
+            tagName->
+            binding?.chipGroup?.addView(createTagChip(requireContext(),tagName))
+        }
+
+    }
+
+    private fun createTagChip(context: Context,chipName:String) : Chip{
+        return Chip(context).apply {
+            text = chipName
+            setChipBackgroundColorResource(R.color.purple_200)
+            isCloseIconVisible=false
+            setTextColor(ContextCompat.getColor(context,R.color.white))
+            id = ViewCompat.generateViewId()
+
+        }
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,4 +63,8 @@ class HomeFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
