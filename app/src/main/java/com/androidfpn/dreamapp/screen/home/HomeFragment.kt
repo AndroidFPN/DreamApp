5b9c83ba16b61ddac9602614146576e99bb772e2
nedajavan747 <nedajavan747@gmail.com>
2022-05-10 21:17:04 +0430

    [Add] kotlin-android-extensions and use it in home fragment for finding views.

--- app/src/main/java/com/androidfpn/dreamapp/screen/home/HomeFragment.kt
+++ app/src/main/java/com/androidfpn/dreamapp/screen/home/HomeFragment.kt
@@ -1,18 +1,17 @@
 package com.androidfpn.dreamapp.screen.home
 
 import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.androidfpn.dreamapp.MyApplication
 import com.androidfpn.dreamapp.R
-import com.androidfpn.dreamapp.databinding.HomeFragmentBinding
 import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : Fragment() {
 
@@ -20,29 +19,22 @@ class HomeFragment : Fragment() {
         fun newInstance() = HomeFragment()
     }
 
-    private var _binding: HomeFragmentBinding? = null
-    private val binding get() = _binding
-
     private val viewModel: HomeViewModel by viewModels {
         HomeViewModel.HomeViewModelFactory((requireActivity().application as MyApplication).soundRepository)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = HomeFragmentBinding.inflate(inflater, container, false)
-        val view = binding?.root
-
-
-        viewModel.chipsData.observe(viewLifecycleOwner) { items ->
-            items.forEach { chipsText ->
-                binding?.chipGroup?.addView(createTagChip(requireContext(), chipsText.name))
-            }
-        }
-
-
-        return view
-
+        return inflater.inflate(R.layout.home_fragment, container, false);
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.chipsData.observe(viewLifecycleOwner) { items ->
+            items.forEach { chipsText ->
+                chipGroup.addView(createTagChip(requireContext(), chipsText.name))
+            }
+        }
+    }
 
     private fun createTagChip(context: Context, chipName: String): Chip {
         return Chip(context).apply {
