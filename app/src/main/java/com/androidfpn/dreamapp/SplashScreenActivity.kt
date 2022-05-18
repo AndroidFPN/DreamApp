c82e634f3d8cb2ead6b760b60b02b820dc62afea
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2022-05-18 12:00:38 +0430

    [NEW]:Add SplashScreen feature

---
+++ app/src/main/java/com/androidfpn/dreamapp/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.androidfpn.dreamapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed(Runnable {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 2000)
+
+
+    }
+
+
+}
